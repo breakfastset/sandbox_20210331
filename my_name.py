998977a5dec7aeab56c91bf10d954509da5be208
breakfastset <kleng99@gmail.com>
2021-03-31 13:04:30 +0800

    Refactor welcome message into greet_user()

--- my_name.py
+++ my_name.py
@@ -1,6 +1,11 @@
-"""Batman"""
+"""Batman and Justice League"""
 
 def main():
+    greet_user()
+
+def greet_user():
     print("The Bat rules")
+    print("... with his friends")
+    print("dedicated to Superman, Wonder Woman, Flash, Aquaman, Cyborg")
 
 main()
