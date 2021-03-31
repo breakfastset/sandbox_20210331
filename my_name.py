e9afff07d2676f5146cdb09dbe9e97af37731d41
breakfastset <kleng99@gmail.com>
2021-03-31 13:09:16 +0800

    Add file saving feature

--- my_name.py
+++ my_name.py
@@ -2,10 +2,15 @@
 
 def main():
     greet_user()
+    save_file()
 
 def greet_user():
     print("The Bat rules")
     print("... with his friends")
     print("dedicated to Superman, Wonder Woman, Flash, Aquaman, Cyborg")
+
+def save_file():
+    print("Saving my friends...")
+
 
 main()
